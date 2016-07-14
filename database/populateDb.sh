e5edb9d511f7b6025196ac89b8c81b68e0755cbb
Michael Baldwin <baldwmic@mail.gvsu.edu>
2016-07-14 14:26:51 -0400

    fixed bug where some tag table names were incorrect

--- database/populateDb.sh
+++ database/populateDb.sh
@@ -19,7 +19,7 @@ directory=$1
 host=$2
 
 # specify order in which data should be inserted since some relies on other relations so sequence is important
-scripts=("reset.sql" "users.sql" "fellows.sql" "companies.sql" "tags.sql" "fellow_tag.sql" "company_tag.sql" "votes_fellows.sql" "votes_companies.sq")
+scripts=("reset.sql" "users.sql" "fellows.sql" "companies.sql" "tags.sql" "fellow_tags.sql" "company_tags.sql" "votes_fellows.sql" "votes_companies.sq")
 for i in "${scripts[@]}"
 do
   sqlFile="${directory}/${i}"
