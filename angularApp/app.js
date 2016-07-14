09eeaaf3a657f232ab73ce464c2e7dc7ad76c154
AndrewHigginbotham <ashiggin@mtu.edu>
2016-07-14 11:31:55 -0400

    Removing unused files. updating homepage and routing

--- angularApp/app.js
+++ angularApp/app.js
@@ -36,20 +36,16 @@
             templateUrl: 'components/profileSingle/profileSingleWrapper.html',
         })
         .when('/about', {
-            controller  : 'static_aboutController',
-            templateUrl : 'components/static_about/static_about.html'
+            templateUrl : 'assets/html/static_about.html'
         })
         .when('/application', {
-            controller  : 'static_applicationController',
-            templateUrl : 'components/static_application/static_application.html'
+            templateUrl : 'assets/html/static_application.html'
         })
         .when('/contact', {
-            controller  : 'static_contact',
-            templateUrl : 'components/static_contact/static_contact.html'
+            templateUrl : 'assets/html/static_contact.html'
         })
         .when('/groups', {
-            controller  : 'static_groups',
-            templateUrl : 'components/static_groups/static_groups.html'
+            templateUrl : 'static_groups.html'
         })
         .when('/dash', {
             controller  : 'DashController',
